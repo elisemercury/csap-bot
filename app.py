8cc85e5e71ba3346d1f4e722c6d793947f10e751
elisemercury <56441652+elisemercury@users.noreply.github.com>
2021-02-05 16:54:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -40,11 +40,8 @@ bot = TeamsBot(
     ]
 )
 
-# url = urlparse(os.environ.get('DATABASE_URL'))
-# db = "dbname=%s user=%s password=%s host=%s " % (url.path[1:], url.username, url.password, url.hostname)
-# schema = "schema.sql"
-# conn = psycopg2.connect(db)
-# cur = conn.cursor()
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 def greeting(incoming_msg):
     fetch_infos(incoming_msg)
