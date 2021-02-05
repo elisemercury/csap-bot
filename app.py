781ff37193d27881a389b609b882301447745740
elisemercury <56441652+elisemercury@users.noreply.github.com>
2021-02-05 23:57:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -86,13 +86,13 @@ def handle_cards(api, incoming_msg):
     db_entry = roomId.id
 
     if m["inputs"] == "subscribe":
-        try:
-            cur.execute("""INSERT INTO subscribers (roomid) VALUES (%s)""", (db_entry,))
-            con.commit()
-            
-            return "Thank you, you sucessfully subscribed to CSAP bot updates."
-        except:
-           return "Thank you, you are already subscribed to CSAP bot updates."
+        #try:
+        cur.execute("""INSERT INTO subscribers (roomid) VALUES (%s)""", (db_entry,))
+        con.commit()
+        return "Thank you, you sucessfully subscribed to CSAP bot updates."
+        #except:
+         #   print("Could not be added to DB")
+          #  return ""
             
             
     if m["inputs"] == "unsubscribe":    
