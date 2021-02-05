d676983ba1ce5e076389abc4aa11c8ab244b1098
elisemercury <56441652+elisemercury@users.noreply.github.com>
2021-02-05 16:27:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import os
 #working
 bot_app_name = "Time Recording Bot"
 bot_token= "N2Y5NTA3NmUtYjc4MC00ZGFhLWE4MjctNDgwOTc4ZjUwMzI2YjI4MDViZTUtOGNk_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f"
-bot_url= "https://csapbot.herokuapp.com/"
+bot_url= "https://csap-bot.herokuapp.com/"
 bot_email = "timerec@webex.bot"
 subscriber_db = "subscribers.txt"
 
